9735daa8c0a6bd59b3b06a6bb502ff06e764c592
Javier Sáez García <javiersaez1205@gmail.com>
2022-10-18 09:51:00 +0200

    public

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-empleados';
+  empleados:Empleado[] = [
+    new Empleado("Javier","Saez","Presidente",7500),
+    new Empleado("Aleth","Doblas","Asesora",6300),
+    new Empleado("Luis","Perez","Pelota",1500),
+    new Empleado("Anatxa","Roger","Recepcionista",1400),
+    new Empleado("Laura","Narvaez","Developer",3000),
+
+  ]
 }
